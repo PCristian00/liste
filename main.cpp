e0649491c0410c5a552482e0a1094adaeae67635
PCristian00 <cristianp9300@gmail.com>
2022-02-07 15:58:00 +0100

    Menu secondari

--- main.cpp
+++ main.cpp
@@ -80,6 +80,19 @@ bool estrai_elem_dato(lista &inz, elem &a) {
     return true;
 }
 
+//Ordina la lista in ordine crescente
+void ordinaLista(lista &inz) {
+    lista q, p;
+    double temp;
+    for (q = inz; q != 0; q = q->succ)
+        for (p = q; p != 0; p = p->succ)
+            if (q->val > p->val) {
+                temp = q->val;
+                q->val = p->val;
+                p->val = temp;
+            }
+}
+
 //Stampa su file scelto la lista
 void stampaFile(lista &inz, char nomefile[]) {
     fstream out;
@@ -94,28 +107,16 @@ void stampaFile(lista &inz, char nomefile[]) {
 
     lista p;
 
-    for (p=inz;p!=0;p=p->succ)
+    for (p = inz; p != 0; p = p->succ)
         out << p->val << endl;
 
     out.close();
 }
 
-//Ordina la lista in ordine crescente
-void ordinaLista(lista &inz){
-    lista q,p;
-    double temp;
-    for(q=inz;q!=0;q=q->succ)
-        for(p=q;p!=0;p=p->succ)
-            if(q->val>p->val){
-                temp=q->val;
-                q->val=p->val;
-                p->val=temp;
-            }
-}
-
 int main() {
 
     int s = 0;
+    int sub = 0;
 
     elem a{};
     lista inizio;
@@ -123,18 +124,15 @@ int main() {
 
     char nomefile[100];
 
-    while (s != 9) {
+    while (s != 6) {
 
         cout << "MENU" << endl;
-        cout << "1 - INS IN TESTA" << endl;
-        cout << "2 - EST DA TESTA" << endl;
-        cout << "3 - INS IN FONDO" << endl;
-        cout << "4 - EST DA FONDO" << endl;
-        cout << "5 - INS ORDINATO" << endl;
-        cout << "6 - EST ELEM DATO" << endl;
-        cout << "7 - ORDINA" <<endl;
-        cout << "8 - STAMPA SU FILE" << endl;
-        cout << "9 - ESCI" << endl;
+        cout << "1 - INSERIMENTO" << endl;
+        cout << "2 - ESTRAZIONE" << endl;
+        cout << "3 - ORDINA LISTA" << endl;
+        cout << "4 - SALVA SU FILE" << endl;
+        cout << "5 - STATISTICHE (WIP)" << endl;
+        cout << "6 - ESCI" << endl;
 
         cout << endl;
         cout << "SCEGLIERE OPERAZIONE" << endl;
@@ -146,54 +144,88 @@ int main() {
             case 1:
                 cout << "INSERIRE VALORE" << endl;
                 cin >> a.val;
-                insTesta(inizio, a);
-                cout << a.val << " INSERITO IN TESTA ALLA LISTA" << endl;
+                cout << "MENU INSERIMENTO" << endl;
+                cout << "1 - TESTA" << endl;
+                cout << "2 - FONDO" << endl;
+                cout << "3 - IN ORDINE" << endl;
+                cout << "DOVE INSERIRE?" << endl;
+
+                cin >> sub;
+
+                switch (sub) {
+                    case 1:
+                        insTesta(inizio, a);
+                        cout << a.val << " INSERITO IN TESTA ALLA LISTA" << endl;
+                        break;
+
+                    case 2:
+                        cout << "INSERIRE VALORE" << endl;
+                        cin >> a.val;
+                        insFondo(inizio, a);
+                        cout << a.val << " INSERITO IN CODA ALLA LISTA" << endl;
+                        break;
+
+                    case 3:
+                        cout << "INSERIRE VALORE" << endl;
+                        cin >> a.val;
+                        insOrdinato(inizio, a);
+                        cout << a.val << " INSERITO NELLA LISTA" << endl;
+                        break;
+
+                    default:
+                        cout << "VALORE NON AMMESSO" << endl;
+                        break;
+                }
+
                 break;
 
             case 2:
-                if (estrai_da_testa(inizio, a)) cout << a.val << " ESTRATTO DALLA LISTA" << endl;
-                else cout << "LISTA VUOTA, ESTRAZIONE FALLITA" << endl;
+                cout << "MENU ESTRAZIONE" << endl;
+                cout << "1 - TESTA" << endl;
+                cout << "2 - FONDO" << endl;
+                cout << "3 - ELEMENTO SCELTO" << endl;
+                cout << "DOVE ESTRARRE?" << endl;
+
+                cin >> sub;
+
+                switch (sub) {
+                    case 1:
+                        if (estrai_da_testa(inizio, a)) cout << a.val << " ESTRATTO DALLA LISTA" << endl;
+                        else cout << "LISTA VUOTA, ESTRAZIONE FALLITA" << endl;
+                        break;
+
+                    case 2:
+                        if (estFondo(inizio, a)) cout << a.val << " ESTRATTO DALLA LISTA" << endl;
+                        else cout << "LISTA VUOTA, ESTRAZIONE FALLITA" << endl;
+                        break;
+
+                    case 3:
+                        cout << "INSERIRE VALORE" << endl;
+                        cin >> a.val;
+                        if (estrai_elem_dato(inizio, a)) cout << a.val << " ESTRATTO DALLA LISTA" << endl;
+                        else cout << a.val << " NON PRESENTE NELLA LISTA, ESTRAZIONE FALLITA" << endl;
+                        break;
+
+                    default:
+                        cout << "VALORE NON AMMESSO" << endl;
+                        break;
+                }
+
                 break;
 
             case 3:
-                cout << "INSERIRE VALORE" << endl;
-                cin >> a.val;
-                insFondo(inizio, a);
-                cout << a.val << " INSERITO IN CODA ALLA LISTA" << endl;
+                ordinaLista(inizio);
+                cout << "ELEMENTI ORDINATI IN ORDINE CRESCENTE" << endl;
                 break;
 
             case 4:
-                if (estFondo(inizio, a)) cout << a.val << " ESTRATTO DALLA LISTA" << endl;
-                else cout << "LISTA VUOTA, ESTRAZIONE FALLITA" << endl;
-                break;
-
-            case 5:
-                cout << "INSERIRE VALORE" << endl;
-                cin >> a.val;
-                insOrdinato(inizio, a);
-                cout << a.val << " INSERITO NELLA LISTA" << endl;
-                break;
-
-            case 6:
-                cout << "INSERIRE VALORE" << endl;
-                cin >> a.val;
-                if (estrai_elem_dato(inizio, a)) cout << a.val << " ESTRATTO DALLA LISTA" << endl;
-                else cout << a.val << " NON PRESENTE NELLA LISTA, ESTRAZIONE FALLITA" << endl;
-                break;
-
-            case 7:
-                ordinaLista(inizio);
-                cout<<"ORDINATI"<<endl;
-                break;
-
-            case 8:
                 cout << "INSERIRE NOME FILE (es. lista.txt)" << endl;
                 cin >> nomefile;
                 stampaFile(inizio, nomefile);
                 cout << "LA LISTA E' STATA SALVATA IN " << nomefile << endl;
                 break;
 
-            case 9:
+            case 6:
                 cout << "USCITA" << endl;
                 break;
 
