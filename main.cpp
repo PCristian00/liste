6f997bb879029b49af88657ca17fe412537d3df4
PCristian00 <cristianp9300@gmail.com>
2022-02-09 11:17:20 +0100

    Maggiori dettagli errore

--- main.cpp
+++ main.cpp
@@ -257,7 +257,15 @@ int main() {
         cout << endl;
     }
 
-    fstream broken; //TODO TOGLIERLO ROMPE IL PROGRAMMA
+    //TODO LEGGERE
+    /*
+     * rimuovere questo fstream crea problemi alle funzioni
+     *
+     * -var-create: unable to create variable object
+     * SIGSEGV (Segmentation fault)
+     *
+     */
+    fstream broken;
 
     return 0;
 
